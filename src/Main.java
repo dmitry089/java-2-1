af170623a2fdadca21b2c792dc7e0e1a042166c0
“dmitry089” <dmitry.karaulov@rambler.ru>
2023-01-13 15:49:38 +0400

    Change commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int  ticket = 3147;
+        double bonusMiles = 20;
+        System.out.println("Количество начисленных миль:");
+        System.out.println(ticket/bonusMiles);
+    }
+}
