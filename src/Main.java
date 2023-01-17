3b227cf78ff449cb6e5fa650f2f2d75d1506ee4a
“dmitry089” <dmitry.karaulov@rambler.ru>
2023-01-17 15:04:51 +0400

    add gitignore

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         int  ticket = 3147;
-        double bonusMiles = 20;
+        int bonusMiles = 20;
         System.out.println("Количество начисленных миль:");
         System.out.println(ticket/bonusMiles); //Вывод бонусов
     }
